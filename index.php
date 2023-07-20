8326562e27f9464ca1e3bfd8fedc0c8f0473a32f
Lucas Silva <lucascattosilva2018@gmail.com>
2023-07-20 13:43:14 -0300

    Criando Painel CMS 1/8

--- index.php
+++ index.php
@@ -6,8 +6,36 @@
         <title>Administrative Panel</title>
         <!-- BootStrap Import -->
         <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <!-- Own css -->
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
+        <!-- Criando Painel CMS 1/8 -->
+        <nav class="navbar navbar-default navbar-fixed-top"> <!-- default -->
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        
+                    <span class="sr-only">Toogle Navigation</span>
+
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand">Administrative Panel</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Register Team</a></li>
+                        <li><a href="#">Edit About</a></li>
+                        <li><a href="#">Manage team</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Log Out</a></li>
+                    </ul>
+                </div>
+            </div> <!-- container -->
+        </nav>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="./js/bootstrap.min.js"></script>
