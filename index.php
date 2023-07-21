e052f3646e6a1bf9d534f60b9ef9b58686574c91
Lucas Silva <lucascattosilva2018@gmail.com>
2023-07-21 15:34:35 -0300

    Criando Painel CMS 3/8

--- index.php
+++ index.php
@@ -54,6 +54,95 @@
             </div> <!-- container -->
         </header>
 
+        <!-- Criando Painel CMS 3/8 -->
+
+        <section class="bread">
+            <div class="container">
+                <ol class="breadcrumb">
+                    <li class="active">Home</li>
+                </ol>
+            </div>
+        </section>
+
+        <section class="principal">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item active cor-padrao">
+                                <span class="glyphicon glyphicon-home"></span> Home
+                            </a>
+                            <a href="#" class="list-group-item"><span class="glyphicon glyphicon-pencil"></span> About</a>
+                            <a href="#" class="list-group-item"><span class="glyphicon glyphicon-pencil"></span> Team<span  class="badge">2</span></a>
+                        </div> <!-- list-group -->
+                    </div> <!-- col-md-3 -->
+                    <div class="col-md-9">
+                        <!-- ---------------------------------------------------------------------------------------------------- -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading cor-padrao">
+                                <h3 class="panel-title">About</h3>
+                            </div> <!-- panel-heading cor-padrao -->
+                            <div class="panel-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="f">Código HTML:</label>
+                                        <textarea id="f" class="form-control"></textarea>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-default">Atualize on Website</button>
+                                </form>
+                            </div> <!-- panel-body -->
+                        </div> <!-- panel panel-default -->
+                        <!-- ---------------------------------------------------------------------------------------------------- -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading cor-padrao">
+                                <h3 class="panel-title">Register Team</h3>
+                            </div> <!-- panel-heading cor-padrao -->
+                            <div class="panel-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="n">Name of member:</label>
+                                        <input type="text" name="member_name" class="form-control">
+                                    </div>
+
+                                    <div class="form-group">
+                                    <label for="d">Description of the member:</label>
+                                        <textarea id="d" class="form-control"></textarea>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-default">Atualize on Website</button>
+                                </form>
+                            </div> <!-- panel-body -->
+                        </div> <!-- panel panel-default -->
+                        <!-- ---------------------------------------------------------------------------------------------------- -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading cor-padrao">
+                                <h3 class="panel-title">Register Team</h3>
+                            </div> <!-- panel-heading cor-padrao -->
+                            <div class="panel-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Id:</th>
+                                            <th>Name:</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Lucas</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div> <!-- panel-body -->
+                        </div> <!-- panel panel-default -->
+                        <!-- ---------------------------------------------------------------------------------------------------- -->
+                    </div> <!-- col-md-9 -->
+                    <!-- ---------------------------------------------------------------------------------------------------- -->
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </section>
+
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="./js/bootstrap.min.js"></script>
